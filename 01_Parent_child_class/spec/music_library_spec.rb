e552d8da5f52ea52d931f8919342daa298bd3c53
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-17 20:16:28 +0100

    add isolated tests, pass tests

--- 01_Parent_child_class/spec/music_library_spec.rb
+++ 01_Parent_child_class/spec/music_library_spec.rb
@@ -1,8 +1,26 @@
 require 'music_library'
 
 RSpec.describe MusicLibrary do
-  context "" do
-    it "" do
+  let(:lib) { MusicLibrary.new }
+  let(:track_3) { double(:track, matches?: true) }
+  let(:track_4) { double(:track, matches?: false) }
+
+  context "isolated tests" do
+    it "returns a list of tracks" do
+      lib.add(track_3)
+      expect(lib.all).to eq [track_3]
+    end
+
+    it "returns a list of tracks" do
+      lib.add(track_3)
+      lib.add(track_4)
+      expect(lib.all).to eq [track_3, track_4]
+    end
+
+    it "returns a list of tracks which matches the keyword" do
+      lib.add(track_3)
+      lib.add(track_4)
+      expect(lib.search("la")).to eq [track_3]
     end
   end
 end
--- 01_Parent_child_class/spec/track_spec.rb
+++ 01_Parent_child_class/spec/track_spec.rb
@@ -1,8 +1,21 @@
 require 'track'
 
 RSpec.describe Track do
-  context "" do
-    it "" do
+  let(:track_1) { Track.new("Title_1", "Artist_1") } 
+  let(:track_2) { Track.new("Lalala", "Something_else") }
+
+  context "isolated tests" do
+    it "returns title" do
+      expect(track_1.title).to eq "Title_1"
+    end
+
+    it "returns artist" do
+      expect(track_2.artist).to eq "Something_else"
+    end
+
+    it "#matches?" do
+      expect(track_2.matches?("la")).to eq true
+      expect(track_1.matches?("la")).to eq false
     end
   end
 end
