41493545d21a48c05ae8cb8e54e36fb47d7e66c4
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-18 15:16:41 +0100

    add methods, add unit tests, add integration test, passes tests

--- 03_peer/lib/secret_diary.rb
+++ 03_peer/lib/secret_diary.rb
@@ -1,21 +1,22 @@
 class SecretDiary
   def initialize(diary) # diary is an instance of Diary
-    # ...
+    @locked = false
+    @diary = diary
   end
 
   def read
-    # Raises the error "Go away!" if the diary is locked
-    # Returns the diary's contents if the diary is unlocked
-    # The diary starts off locked
+    if @locked == false
+      @diary
+    else
+      return "Go away spy!"
+    end
   end
 
   def lock
-    # Locks the diary
-    # Returns nothing
+    @locked = true
   end
 
   def unlock
-    # Unlocks the diary
-    # Returns nothing
+    @locked = false
   end
 end
